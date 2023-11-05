a020357706a730e3af1a27a3794b0466bae39834
Nelson Soto <nelson.soto.c05@gmail.com>
2023-11-05 01:01:49 -0400

    Add replace static data to data response by api

--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 import { IssueList } from '../components/IssueList';
 import { LabelPicker } from '../components/LabelPicker';
+import { useIssues } from '../../hooks';
+import { LoadingIcon } from '../../shared/components/LoadingIcon';
 
 
 export const ListView = () => {
   const [selectedLabels, setSeletedLabels] = useState<string[]>([]);
+  const issuesQuery = useIssues();
 
-  const onLabelChange  = (labelName: string) => {
+  const onLabelChange = (labelName: string) => {
     setSeletedLabels((prev) => {
       if (prev.includes(labelName)) {
         return prev.filter((label) => label !== labelName);
@@ -17,13 +20,14 @@ export const ListView = () => {
 
   return (
     <div className="row mt-5">
-      
       <div className="col-8">
-        <IssueList />
+        {issuesQuery.isLoading ? <LoadingIcon />
+          : <IssueList issues={issuesQuery.data || []} />
+        }
       </div>
-      
+
       <div className="col-4">
-        <LabelPicker 
+        <LabelPicker
           selectedLabels={selectedLabels}
           onChange={onLabelChange}
         />
