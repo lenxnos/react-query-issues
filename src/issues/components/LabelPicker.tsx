e0f14ee906a78d35c67ffabbf3f6967ee746dadf
Nelson Soto <nelson.soto.c05@gmail.com>
2023-11-04 23:00:18 -0400

    Add implementation get labels including a loading

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,23 @@
+import { useLabels } from '../../hooks';
+import { LoadingIcon } from '../../shared/components/LoadingIcon';
 
 export const LabelPicker = () => {
+  const labelsQuery = useLabels();
+
+  if (labelsQuery.isLoading) return <LoadingIcon />;
+
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
+      {labelsQuery.data?.map((label) => (
+        <span
+          key={label.id}
+          className="badge rounded-pill m-1 label-picker"
+          style={{ border: `1px solid #${label.color}`, color: `#${label.color}` }}
         >
-            Primary
+          {label.name}
         </span>
-        
+      ))}
+
     </div>
   )
 }
