9641f1c270ca0495c421d8e5714a1738bdae4a48
Nelson Soto <nelson.soto.c05@gmail.com>
2023-11-04 23:16:05 -0400

    Add a placeholder labels

--- src/hooks/useLabels.ts
+++ src/hooks/useLabels.ts
@@ -8,5 +8,28 @@ const getLabels = async (): Promise<Label[]> => {
 };
 
 export const useLabels = () => {
-  return useQuery({ queryKey: ['labels'], queryFn: getLabels, refetchOnWindowFocus: false, staleTime: 1000 * 60 * 60 });
+  return useQuery({
+    queryKey: ['labels'],
+    queryFn: getLabels,
+    refetchOnWindowFocus: false,
+    staleTime: 1000 * 60 * 60,
+    placeholderData: [{
+      "id": 196858374,
+      "node_id": "MDU6TGFiZWwxOTY4NTgzNzQ=",
+      "url": "https://api.github.com/repos/facebook/react/labels/CLA%20Signed",
+      "name": "CLA Signed",
+      "color": "e7e7e7",
+      "default": false,
+      "description": null
+    },
+    {
+      "id": 69105358,
+      "node_id": "MDU6TGFiZWw2OTEwNTM1OA==",
+      "url": "https://api.github.com/repos/facebook/react/labels/Browser:%20Safari",
+      "name": "Browser: Safari",
+      "color": "c7def8",
+      "default": false,
+      "description": null
+    }]
+  });
 };
